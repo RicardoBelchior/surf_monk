52f436aa7e89fd6f61c237077a6fc4f7bc3cac41
Ricardo Belchior <ricardolagido@gmail.com>
2013-11-17 14:52:26 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurfMonkApp::Application.config.secret_key_base = 'c9b96e3d36f0919562831fdf2520cb3440b7edeb9fea2be62cbf78c68a9fad0be7262cb6ea49291559925e7bc00c5f614efba30201ba461389a8a2537f264399'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SurfMonkApp::Application.config.session_store :cookie_store, key: '_surf_monk_app_session'
